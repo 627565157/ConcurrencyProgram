041d77619b8afbc9f3a7810747f0bbac63537aee
chensi <xiaosi.6666@gmail.com>
2019-02-24 21:51:34 +0800

    读写锁使用示例

---
+++ src/com/chensi/concurrency/demo/CacheOfReadWriteLockDemo.java
@@ -0,0 +1,46 @@
+package com.chensi.concurrency.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 使用读写锁实现一个缓存的使用示例
+ * author:chensi
+ * Date:2019/2/24
+ **/
+public class CacheOfReadWriteLockDemo {
+    static Map<String, Object> map = new HashMap<>();
+    static ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+    static Lock r = rwl.readLock();
+    static Lock w = rwl.writeLock();
+    // 获取一个key对应的value
+    public static final Object get(String key) {
+        r.lock();
+        try {
+            return map.get(key);
+        } finally {
+            r.unlock();
+        }
+    }
+
+    public static final Object put(String key, Object value) {
+        w.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            w.unlock();
+        }
+    }
+
+    //清空所有的内容
+    public static final void clear() {
+        w.lock();
+        try {
+            map.clear();
+        } finally {
+            w.unlock();
+        }
+    }
+}
